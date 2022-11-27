8d84d7acf7b7604f09997613d282e938e5891e86
conorkeane <g00391846@atu.ie>
2022-11-27 15:39:12 +0000

    Added constructor to DeliveryDriver class

--- src/main/java/ie/atu/food_delivery_app/DeliveryDriver.java
+++ src/main/java/ie/atu/food_delivery_app/DeliveryDriver.java
@@ -1,4 +1,10 @@
 package ie.atu.food_delivery_app;
 
 public class DeliveryDriver {
+    private String Title;
+    private String Name;
+    private String ID;
+    private String CarReg;
+    private String LicenceNumber;
+    private int Age;
 }
