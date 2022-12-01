38ef21b90df8f4f72ae9768e52010424d2eb4b70
conorkeane <g00391846@atu.ie>
2022-12-01 16:17:25 +0000

    Added Test for Delivery Driver

--- src/test/java/ie/atu/food_delivery_app/DeliveryDriverTestSuccesses.java
+++ src/test/java/ie/atu/food_delivery_app/DeliveryDriverTestSuccesses.java
@@ -1,12 +1,38 @@
+/*
 package ie.atu.food_delivery_app;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-public class DeliveryDriverTestSuccesses {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DeliveryDriverTestSuccesses {
+    //Test
+    DeliveryDriver myDelivDriver;
 
     @BeforeEach
     void setUp() {
     }
 
+    @Test
+    void success_test_Title() {
+        DeliveryDriver myDelivDriver = new DeliveryDriver("Mr", "Conor", "1111111111", "879543461", 21);
+        assertEquals("Mr", myDelivDriver.getTitle());
+    }
+
+    @Test
+    void success_test_Title_Mrs() {
+        DeliveryDriver myDelivDriver = new DeliveryDriver("Mrs", "Conor", "1111111111", "879543461", 21);
+        assertEquals("Mrs", myDelivDriver.getTitle());
+    }
+
+    @Test
+    void success_test_Title_Ms() {
+        DeliveryDriver myDelivDriver = new DeliveryDriver("Ms", "Conor", "1111111111", "879543461", 21);
+        assertEquals("Ms", myDelivDriver.getTitle());
+    }
+
+
 
 }
+*/
