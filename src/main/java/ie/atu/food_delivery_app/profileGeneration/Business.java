557117c3881ae97ea3830f4428d477d19201fe81
Allyn <G00392765@atu.ie>
2022-12-04 01:24:25 +0000

    Created setters in the business class

--- src/main/java/ie/atu/food_delivery_app/profileGeneration/Business.java
+++ src/main/java/ie/atu/food_delivery_app/profileGeneration/Business.java
@@ -14,4 +14,49 @@ public class Business {
         this.businessOpenTime = businessOpenTime;
         this.businessCloseTime = businessCloseTime;
     }
+
+    public void setBusinessName(String businessName){
+        if(businessName.length() >= 4){
+            this.businessName = businessName;
+        }
+        else{
+            throw new IllegalArgumentException("Business name must have at least 4 characters");
+        }
+    }
+
+    public void setBusinessAddress(String businessAddress){
+        if(businessAddress.length() >= 8){
+            this.businessAddress = businessAddress;
+        }
+        else{
+            throw new IllegalArgumentException("Business address must have at least 8 characters");
+        }
+    }
+
+    public void setBusinessPhoneNo(String businessPhoneNo){
+        if(businessPhoneNo.length() >= 9){
+            this.businessPhoneNo = businessPhoneNo;
+        }
+        else{
+            throw new IllegalArgumentException("Business phone number must have at least 9 digits");
+        }
+    }
+
+    public void setBusinessOpenTime(int businessOpenTime){
+        if(businessOpenTime >= 0 && businessOpenTime <= 2359){
+            this.businessOpenTime = businessOpenTime;
+        }
+        else{
+            throw new IllegalArgumentException("Business opening time must be a valid 24hr time");
+        }
+    }
+
+    public void setBusinessCloseTime(int businessCloseTime){
+        if(businessCloseTime >= 0 && businessCloseTime <= 2359){
+            this.businessCloseTime = businessCloseTime;
+        }
+        else{
+            throw new IllegalArgumentException("Business closing time must be a valid 24hr time");
+        }
+    }
 }
