3684805c50ac40c852d97af3a336b39be293a781
Allyn <G00392765@atu.ie>
2022-12-04 00:43:07 +0000

    Created business class & instance variables

---
+++ src/main/java/ie/atu/food_delivery_app/profileGeneration/Business.java
@@ -0,0 +1,9 @@
+package ie.atu.food_delivery_app.profileGeneration;
+
+public class Business {
+    private String businessName;
+    private String businessAddress;
+    private String businessPhoneNo;
+    private int businessOpenTime;
+    private int businessCloseTime;
+}
