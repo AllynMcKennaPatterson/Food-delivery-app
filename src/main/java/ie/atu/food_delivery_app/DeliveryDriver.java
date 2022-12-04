75c86fa0380f660b1ba00a74abd38ee336f8a71c
conorkeane <g00391846@atu.ie>
2022-12-04 14:18:03 +0000

    Added Tests for title of the delivery driver

--- src/main/java/ie/atu/food_delivery_app/DeliveryDriver.java
+++ src/main/java/ie/atu/food_delivery_app/DeliveryDriver.java
@@ -1,5 +1,15 @@
 package ie.atu.food_delivery_app;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+
 public class DeliveryDriver {
     private String Title;
     private String Name;
@@ -22,5 +32,6 @@ public class DeliveryDriver {
     public String getTitle() {
         return Title;
     }
+
  //test
 }
