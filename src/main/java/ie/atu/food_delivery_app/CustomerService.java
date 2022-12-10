1164501b971f5d28a5a5633e5de8cb38b051114e
Patryk Milkiewicz <g00397265@atu.ie>
2022-12-10 12:56:18 +0000

    - Added CustomerController Class - In CustomerController added GET and POST mapping to retrieve list of Customers and add customer to the list _ In CustomerService Class added function to find all passengers in the list and another to save new customers information

--- src/main/java/ie/atu/food_delivery_app/CustomerService.java
+++ src/main/java/ie/atu/food_delivery_app/CustomerService.java
@@ -1,4 +1,19 @@
 package ie.atu.food_delivery_app;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
 public class CustomerService {
+
+    private final CustomerRepo customerRepo;
+
+    public List<Customer> getCustomers() {
+        return customerRepo.findAll();
+    }
+
+    public void saveCustomer(Customer customer){customerRepo.save(customer);}
 }
