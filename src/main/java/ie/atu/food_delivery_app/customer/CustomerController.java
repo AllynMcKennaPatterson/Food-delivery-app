6cfa60a0d99723dbd87b82df32b462710cea7a18
Allyn <G00392765@atu.ie>
2022-12-11 23:32:52 +0000

    Added getCustomer by id controller

--- src/main/java/ie/atu/food_delivery_app/customer/CustomerController.java
+++ src/main/java/ie/atu/food_delivery_app/customer/CustomerController.java
@@ -3,6 +3,7 @@ package ie.atu.food_delivery_app.customer;
 import ie.atu.food_delivery_app.business.Business;
 import ie.atu.food_delivery_app.business.BusinessService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,11 @@ public class CustomerController {
     @GetMapping
     public List<Customer> getCustomers(){
         return myService.getCustomers();
+    }
+
+
+    @GetMapping("/{customerID}")
+    public Customer getCustomers(@PathVariable String customerID){
+        return myService.getCustomers(customerID);
     }
 }
