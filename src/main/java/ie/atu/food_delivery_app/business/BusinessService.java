104963a0d3ce5e15303447426a57d250f3894e0f
Allyn <G00392765@atu.ie>
2022-12-11 22:54:15 +0000

    Added services to BusinessService class

--- src/main/java/ie/atu/food_delivery_app/business/BusinessService.java
+++ src/main/java/ie/atu/food_delivery_app/business/BusinessService.java
@@ -1,4 +1,24 @@
 package ie.atu.food_delivery_app.business;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
 public class BusinessService {
+
+    private final BusinessRepo businessRepo;
+
+    public List<BusinessRepo> getBusinesses()
+    {
+        return businessRepo.findAll();
+    }
+
+    public Business getBusinesses(String businessID){
+        Business myBusinessUsers = new Business("Dominos", "16 Prospect Hill", "091556100", 1200, 2300);
+        return myBusinessUsers;
+    }
+    public void saveBusinessUser(Business businessUser) {
+        businessRepo.save(user);
+    }
 }
