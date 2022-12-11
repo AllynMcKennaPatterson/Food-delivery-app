46f0f13468c8b9675afc70741b659afb52c38bdb
Allyn <G00392765@atu.ie>
2022-12-11 23:31:40 +0000

    Added getCustomers controller

--- src/main/java/ie/atu/food_delivery_app/customer/CustomerController.java
+++ src/main/java/ie/atu/food_delivery_app/customer/CustomerController.java
@@ -1,4 +1,24 @@
 package ie.atu.food_delivery_app.customer;
 
+import ie.atu.food_delivery_app.business.Business;
+import ie.atu.food_delivery_app.business.BusinessService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="api/customer")
 public class CustomerController {
+    CustomerService myService;
+
+    public CustomerController(CustomerService myService) {
+        this.myService = myService;
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return myService.getCustomers();
+    }
 }
