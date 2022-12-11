16e74d1483521f3192ab1f9e0703811827f4ceff
Allyn <G00392765@atu.ie>
2022-12-11 23:34:12 +0000

    Added saveCustomer controller

--- src/main/java/ie/atu/food_delivery_app/customer/CustomerController.java
+++ src/main/java/ie/atu/food_delivery_app/customer/CustomerController.java
@@ -2,10 +2,7 @@ package ie.atu.food_delivery_app.customer;
 
 import ie.atu.food_delivery_app.business.Business;
 import ie.atu.food_delivery_app.business.BusinessService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,9 +20,13 @@ public class CustomerController {
         return myService.getCustomers();
     }
 
-
     @GetMapping("/{customerID}")
     public Customer getCustomers(@PathVariable String customerID){
         return myService.getCustomers(customerID);
+    }
+
+    @PostMapping
+    public void saveCustomerUser(@RequestBody Customer customerUser){
+        myService.saveCustomerUser(customerUser);
     }
 }
