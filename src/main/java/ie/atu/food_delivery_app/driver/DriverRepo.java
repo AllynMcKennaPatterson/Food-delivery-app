c37c255be2b04b4dc3a6f11d9effbc8bf607dcfe
Allyn <G00392765@atu.ie>
2022-12-11 23:39:04 +0000

    Created DriverRepo class. Added autogenerated count to DeliveryDriver class

---
+++ src/main/java/ie/atu/food_delivery_app/driver/DriverRepo.java
@@ -0,0 +1,7 @@
+package ie.atu.food_delivery_app.driver;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DriverRepo extends JpaRepository<DeliveryDriver, Long> {
+
+}
