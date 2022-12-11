b74118398e4c781ad07766ef778409d284306c48
Allyn <G00392765@atu.ie>
2022-12-11 22:45:10 +0000

    Added spring mapping to BusinessController class

--- src/main/java/ie/atu/food_delivery_app/business/BusinessController.java
+++ src/main/java/ie/atu/food_delivery_app/business/BusinessController.java
@@ -1,4 +1,28 @@
 package ie.atu.food_delivery_app.business;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="api/business")
 public class BusinessController {
+    BusinessService myService;
+
+    public BusinessController(BusinessService myService) {
+        this.myService = myService;
+    }
+
+    @GetMapping
+    public List<Business> getBusinesses(){
+        return myService.getBusinesses();
+    }
+
+    @GetMapping("/{businessID}")
+    public Bank getBusinesses(@PathVariable String businessID){
+        return myService.getBusinesses(businessID);
+    }
+
+    @PostMapping
+    public void saveBusinessUser(@RequestBody Business businessUser){
+        myService.saveBusinessUser(businessUser);
+    }
 }
