4915959e54f789cd67a6221cb728b178edeb1aad
Allyn <G00392765@atu.ie>
2022-12-11 23:47:42 +0000

    Added service to return all delivery drivers

--- src/main/java/ie/atu/food_delivery_app/driver/DeliveryDriverService.java
+++ src/main/java/ie/atu/food_delivery_app/driver/DeliveryDriverService.java
@@ -1,4 +1,20 @@
 package ie.atu.food_delivery_app.driver;
 
+import ie.atu.food_delivery_app.customer.Customer;
+import ie.atu.food_delivery_app.customer.CustomerRepo;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
 public class DeliveryDriverService {
+
+    private final DeliveryDriverRepo deliveryDriverRepo;
+
+    public List<DeliveryDriver> getDeliveryDrivers()
+    {
+        return deliveryDriverRepo.findAll();
+    }
 }
