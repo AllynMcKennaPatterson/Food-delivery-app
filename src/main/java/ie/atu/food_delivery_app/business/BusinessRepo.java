596ea3126a29c0171c625511b162ee650d2724ad
Allyn <G00392765@atu.ie>
2022-12-11 22:56:19 +0000

    Created BusinessRepo interface

---
+++ src/main/java/ie/atu/food_delivery_app/business/BusinessRepo.java
@@ -0,0 +1,6 @@
+package ie.atu.food_delivery_app.business;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BusinessRepo extends JpaRepository<Business, Long> {
+}
