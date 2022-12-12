78590d60b13db9a68897be1d50ebb1b98b8b1581
Patryk Milkiewicz <g00397265@atu.ie>
2022-12-12 10:08:13 +0000

    Removed JPA folder

--- src/main/java/ie/atu/food_delivery_app/JPA/CustomerRepo.java
+++
@@ -1,8 +0,0 @@
-package ie.atu.food_delivery_app.JPA;
-
-import ie.atu.food_delivery_app.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepo extends JpaRepository<Customer, Long> {
-    Customer findCustomerByName(String name);
-}
