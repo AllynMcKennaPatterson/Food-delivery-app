13a4cdef24535bb9074bc2cc20ce90c281fb66c5
conorkeane <g00391846@atu.ie>
2022-12-12 12:26:11 +0000

    Added @Getter, @Setter, @NoArgsConstructor and etc to Delivery Driver class

--- src/main/java/ie/atu/food_delivery_app/driver/DeliveryDriver.java
+++ src/main/java/ie/atu/food_delivery_app/driver/DeliveryDriver.java
@@ -5,13 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-//@AllArgsConstructor
-
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
 
 public class DeliveryDriver {
     private String Title;
